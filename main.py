4dd2669f2e2c15124a4c765b74b113c76f4b6cc4
Caíque S. Gonçalves <caique.goncalves@pucpr.edu.br>
2026-03-21 13:37:22 -0300

    Refiz o código adicionando o while true, e um novo comando "ADD"

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-comando = input("Digite um comando [ABOUT/QUIT]: ").upper()
+while True:
+    comando = input("Digite um comando [ABOUT/QUIT/ADD]: ").upper()
+    match comando:
+        case "ABOUT":
+            print("Seja bem vindo ao Gestor de Portfólio do Caíque.")
+        case "QUIT":
+            print("Saindo do Gestor de Portfólio.")
+            break
+        case "ADD":
+            while True:
+                quantidade_projeto = int(input("Informe quantos projetos deseja adicionar: "))
+                if quantidade_projeto <= 0:
+                    print("A quantidade de projetos não pode ser menor ou igual a zero!")
+                else:
+                    for i in range(quantidade_projeto):
+                        nome_projeto = input("Digite o nome do projeto: ")
+                        print(f"SUCESSO: Projeto '{nome_projeto}' adicionado.")
+                    break
+        case _:
+            print("ERRO: Comando não reconhecido.")
 
-match comando:
-    case "ABOUT":
-        print("Seja bem vindo ao Gestor de Portfólio do Caíque.")
-    case "QUIT":
-        print("Saindo do Gestor de Portfólio.")
-    case _:
-        print("ERRO: Comando não reconhecido.")
 print("Até logo!!!")
-
-
-
